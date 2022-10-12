c93f03b065b5e940bc744be1ac48157c80bd4b05
Ihor <unighteal@gmail.com>
2022-10-12 17:57:51 +0300

    Update failure.php

--- sample/failure.php
+++ sample/failure.php
@@ -1,6 +1,6 @@
 <?php require_once __DIR__.'/response.php'; ?>
 <!DOCTYPE html>
-<html<html lang="en">
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
